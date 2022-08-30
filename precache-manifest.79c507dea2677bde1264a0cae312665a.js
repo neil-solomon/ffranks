15590f22fd9cb3c343946e063f58e60ba3f278a6
NeilSolomon <neilsolomon89@gmail.com>
2022-08-29 22:29:31 -0400

    Updates

---
+++ precache-manifest.79c507dea2677bde1264a0cae312665a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e44422f51cfd02279f081a4dfac1074",
+    "url": "/ffranks/index.html"
+  },
+  {
+    "revision": "12a1a39dac0cc57e168d",
+    "url": "/ffranks/static/css/main.baf65062.chunk.css"
+  },
+  {
+    "revision": "f281cb5af27e93438772",
+    "url": "/ffranks/static/js/2.ba9950f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ffranks/static/js/2.ba9950f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a1a39dac0cc57e168d",
+    "url": "/ffranks/static/js/main.63038b64.chunk.js"
+  },
+  {
+    "revision": "fecde3229373a7172d89",
+    "url": "/ffranks/static/js/runtime-main.d25cbd49.js"
+  }
+]);
